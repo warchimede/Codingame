55d6b4fcd847c425bba12366379ac85df978bc8d
William Archimede <william.archimede@gmail.com>
2021-07-11 16:33:33 +0200

    python: Add the-descent

---
+++ the-descent.py
@@ -0,0 +1,15 @@
+import sys
+import math
+
+# game loop
+while True:
+    index = 0
+    height = 0
+
+    for i in range(8):
+        mountain_h = int(input())  # represents the height of one mountain.
+        if (mountain_h > height):
+            height = mountain_h
+            index = i
+    
+    print(index)
